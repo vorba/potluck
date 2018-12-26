6692e5381a428fc7663dbbc39fbe6000814fc007
Adam Cox <acox@actra.ca>
2018-12-25 21:52:30 -0500

    add google maps

--- src/pages/menu-detail/menu-detail.ts
+++ src/pages/menu-detail/menu-detail.ts
@@ -1,12 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MenuDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+declare var google;
 
 @IonicPage()
 @Component({
@@ -31,11 +26,29 @@ export class MenuDetailPage {
     }
   }
 
+  @ViewChild('map') map: ElementRef;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuDetailPage');
+
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.map.nativeElement, mapOptions);
+ 
   }
 
 }
